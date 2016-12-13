3463cc182034d4bcf9833bd231e90f917fcbd2c7
Justin Inabnitt <jinabnitt@gmail.com>
2016-12-12 17:06:08 -0700

    add initial files

---
+++ dance.js
@@ -0,0 +1,12 @@
+
+
+ function pointLeft() {
+   console.log("Point Left");
+ }
+ function pointRight() {
+   console.log("Point Right");
+ }
+ for (var i=0; i<2; i++) {
+   pointLeft();
+   pointRight();
+ };
